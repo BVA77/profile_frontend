40091b83305744a680013ddba0b30859adc2e9c0
unknown <varicha.auc7@gmail.com>
2022-12-09 01:45:42 +0700

    update info content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/pages/home/home.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { InfoComponent } from './shared/components/info/info.component';
+import { AboutComponent } from './shared/components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    InfoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
